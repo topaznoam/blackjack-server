9fdcb8c647d2d8e310d5a5b914e9bd4ed16e8a4e
topaznoam <noamtopaz@gmail.com>
2024-07-12 14:31:01 +0300

    Resolve merge conflicts

--- src/ManageGame.ts
+++ src/ManageGame.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Card, calcHand, hasAce } from "./Cards";
 
 export function checkForWinner(player: Card[], dealer: Card[]) {
@@ -6,13 +5,6 @@ export function checkForWinner(player: Card[], dealer: Card[]) {
   const dealerPoints = calcHand(dealer);
   let playerHasJoker = false;
   let dealerHasJoker = false;
-=======
-import { Card, CalcHand, HasAce } from "./Cards";
-
-export function CheckForWinner(player: Card[], dealer: Card) {
-  const playerPoints = CalcHand(player);
-  let playerHasJoker = false;
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
 
   for (const card of player) {
     if (card.isjoker) {
@@ -20,7 +12,6 @@ export function CheckForWinner(player: Card[], dealer: Card) {
       break;
     }
   }
-<<<<<<< HEAD
   for (const card of dealer) {
     if (card.isjoker) {
       dealerHasJoker = true;
@@ -31,21 +22,11 @@ export function CheckForWinner(player: Card[], dealer: Card) {
   if (dealerHasJoker && playerHasJoker) {
     return "Tie";
   } else if (dealerHasJoker) {
-=======
-
-  if (dealer.isjoker && playerHasJoker) {
-    return "Tie";
-  } else if (dealer.isjoker) {
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
     return "Dealer wins";
   } else if (playerHasJoker) {
     return "Player wins";
   } else if (playerPoints > 21) {
-<<<<<<< HEAD
     if (hasAce(player)) {
-=======
-    if (HasAce(player)) {
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
       for (const card of player) {
         const rank = card.name.split("_of_")[0];
         if (rank === "ace") {
@@ -59,30 +40,20 @@ export function CheckForWinner(player: Card[], dealer: Card) {
     }
   } else if (playerPoints === 21) {
     return "Player wins";
-<<<<<<< HEAD
   } else if (dealerPoints === 21) {
     return "Dealer wins";
-=======
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
   } else {
     return null;
   }
 }
 
-<<<<<<< HEAD
 export const whoWon = (player: Card[], dealer: Card[]) => {
   const playerPoints = calcHand(player);
   const dealerPoints = calcHand(dealer);
-=======
-export const HowWon = (player: Card[], dealer: Card[]) => {
-  const playerPoints = CalcHand(player);
-  const dealerPoints = CalcHand(dealer);
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
 
   if (playerPoints > 21) {
     return "Dealer wins";
   } else if (dealerPoints > 21) {
-<<<<<<< HEAD
     if (hasAce(dealer)) {
       for (const card of dealer) {
         const rank = card.name.split("_of_")[0];
@@ -95,9 +66,6 @@ export const HowWon = (player: Card[], dealer: Card[]) => {
     } else {
       return "Player wins";
     }
-=======
-    return "Player wins";
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
   } else if (playerPoints > dealerPoints) {
     return "Player wins";
   } else if (dealerPoints > playerPoints) {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-<<<<<<< HEAD
 import { createDeck, calcHand, Card } from "./Cards";
 import { whoWon, checkForWinner } from "./ManageGame";
 
@@ -12,10 +11,6 @@ const port = 5000;
 let deck: Card[] = createDeck();
 let dealerCards: Card[] = [];
 let playerCards: Card[] = [];
-=======
-import { CreateDeck, CalcHand, Card } from "./Cards";
-import { HowWon, CheckForWinner } from "./ManageGame";
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
 
 function getRndInteger(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -24,7 +19,6 @@ function getRndInteger(min: number, max: number) {
 app.use(cors());
 
 app.get("/startgame", (req: Request, res: Response) => {
-<<<<<<< HEAD
   playerCards = [];
   dealerCards = [];
   if (deck.length < MIN_CARDS_IN_DECK) {
@@ -120,79 +114,9 @@ app.get("/stand", (req: Request, res: Response) => {
       sumCardsInDeck: sumCardsInDeck,
     };
     res.json(responseData);
-=======
-  player = [];
-  dealer = [];
-  deck = CreateDeck();
-  for (let i = 0; i < 2; i++) {
-    let cardIndex = getRndInteger(0, deck.length - 1);
-    player.push(deck.splice(cardIndex, 1)[0]);
-
-    cardIndex = getRndInteger(0, deck.length - 1);
-    dealer.push(deck.splice(cardIndex, 1)[0]);
->>>>>>> 0d18e9f03e582a724a4826ebdd0447ebaf77203c
   }
-
-  const playerCardNames = [player[0].name, player[1].name];
-  const dealerCardName = dealer[0].name;
-  const winner = CheckForWinner(player, dealer[0]);
-  const playerPoints = CalcHand(player);
-  let dealerCard;
-  let delerPoints;
-  if (winner) {
-    dealerCard = dealer[1].name;
-    delerPoints = CalcHand(dealer);
-  } else {
-    dealerCard = null;
-    delerPoints = null;
-  }
-  const responseData = {
-    playerCards: playerCardNames,
-    playerPoints: playerPoints,
-    dealerCard: dealerCardName,
-    howisthewinner: winner,
-    dealerfirstcard: dealerCard,
-    delerPoints: delerPoints,
-  };
-
-  res.json(responseData);
 });
-app.get("/getcard", (req: Request, res: Response) => {
-  let cardIndex = getRndInteger(0, deck.length - 1);
-  player.push(deck.splice(cardIndex, 1)[0]);
-  const playerCardName = player[player.length - 1].name;
-  const winner = CheckForWinner(player, dealer[0]);
-  const playerPoints = CalcHand(player);
-  let dealerCard;
-  let delerPoints;
-  if (winner) {
-    dealerCard = dealer[1].name;
-    delerPoints = CalcHand(dealer);
-  } else {
-    dealerCard = null;
-    delerPoints = null;
-  }
-  const responseData = {
-    playerCard: playerCardName,
-    playerPoints: playerPoints,
-    howisthewinner: winner,
-    dealerfirstcard: dealerCard,
-    delerPoints: delerPoints,
-  };
 
-  res.json(responseData);
-});
-app.get("/stand", (req: Request, res: Response) => {
-  const winner = HowWon(player, dealer);
-  const dealerCard = dealer[1].name;
-  const delerPoints = CalcHand(dealer);
-  const responseData = {
-    howisthewinner: winner,
-    dealerfirstcard: dealerCard,
-    delerPoints: delerPoints,
-  };
-  res.json(responseData);
-});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
